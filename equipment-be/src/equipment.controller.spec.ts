016dd0bcb4b4b293efceed52974f8d1d7c8519c4
Amrita Matta <matta.amrita@gmail.com>
2022-03-30 22:11:41 +0300

    added unit tests

--- equipment-be/src/equipment.controller.spec.ts
+++ equipment-be/src/equipment.controller.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -33,54 +34,55 @@ describe('EquipmentController', () => {
     );
   });
 
+  const mockEquipment = {
+    equipmentNumber: 1,
+    address: '',
+    contractStartDate: new Date(),
+    contractEndDate: new Date(),
+    status: EquipmentStatus.Running,
+  };
+
   describe('searchEquipments', () => {
     it('should work', async () => {
-      jest.spyOn(equipmentRepository, 'find').mockImplementationOnce(() =>
-        Promise.resolve([
-          {
-            equipmentNumber: 1,
-            address: '',
-            contractStartDate: new Date(),
-            contractEndDate: new Date(),
-            status: EquipmentStatus.Running,
-          },
-        ]),
-      );
+      jest
+        .spyOn(equipmentRepository, 'find')
+        .mockImplementationOnce(() => Promise.resolve([mockEquipment]));
       const results = await controller.searchEquipments(10);
-      expect(results).toEqual([
-        {
-          equipmentNumber: 1,
-          address: '',
-          contractStartDate: new Date(),
-          contractEndDate: new Date(),
-          status: EquipmentStatus.Running,
-        },
-      ]);
+      expect(results).toEqual([mockEquipment]);
     });
   });
-  describe('searchEquipments', () => {
-    it('should work', async () => {
-      jest.spyOn(equipmentRepository, 'find').mockImplementationOnce(() =>
-        Promise.resolve([
-          {
-            equipmentNumber: 1,
-            address: '',
-            contractStartDate: new Date(),
-            contractEndDate: new Date(),
-            status: EquipmentStatus.Running,
-          },
-        ]),
+  describe('getEquipment', () => {
+    it('should return equipment details when it is found', async () => {
+      jest
+        .spyOn(equipmentRepository, 'findOne')
+        .mockImplementationOnce(() => Promise.resolve(mockEquipment));
+      const results = await controller.getEquipment(1);
+      expect(results).toEqual(mockEquipment);
+    });
+
+    it('should throw exception when equipment is not found', async () => {
+      await expect(() => controller.getEquipment(12)).rejects.toThrow(
+        new NotFoundException('EquipmentNumber=12 not found'),
       );
-      const results = await controller.searchEquipments(10);
-      expect(results).toEqual([
-        {
-          equipmentNumber: 1,
-          address: '',
-          contractStartDate: new Date(),
-          contractEndDate: new Date(),
-          status: EquipmentStatus.Running,
-        },
-      ]);
+    });
+  });
+  describe('createEquipment', () => {
+    it('should create new equipment', async () => {
+      const createEquipmentDto = {
+        address: '11 Vantaa',
+        contractStartDate: new Date(),
+        contractEndDate: new Date(),
+        status: EquipmentStatus.Stopped,
+      };
+      const equipment = {
+        equipmentNumber: 11,
+        ...createEquipmentDto,
+      };
+      jest
+        .spyOn(equipmentRepository, 'save')
+        .mockImplementationOnce(() => Promise.resolve(equipment));
+      const results = await controller.createEquipment(createEquipmentDto);
+      expect(results).toEqual(equipment);
     });
   });
 });
