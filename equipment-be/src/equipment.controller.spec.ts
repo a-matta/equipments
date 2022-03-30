3d1e96388eb53093dfd66cea02918f19daf4c91f
Amrita Matta <matta.amrita@gmail.com>
2022-03-30 17:56:20 +0300

    added test case for search equipment

--- equipment-be/src/equipment.controller.spec.ts
+++ equipment-be/src/equipment.controller.spec.ts
@@ -1,35 +1,86 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { EquipmentController } from './equipment.controller';
-import { AppService } from './equipment.service';
+import { Equipment } from './equipment.entity';
+import { EquipmentStatus } from './equipment.status';
 
 describe('EquipmentController', () => {
   let app: TestingModule;
+  let controller: EquipmentController;
+  let equipmentRepository: Repository<Equipment>;
+
+  const EQUIPMENT_REPOSITORY_TOKEN = getRepositoryToken(Equipment);
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [EquipmentController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: EQUIPMENT_REPOSITORY_TOKEN,
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
+
+    controller = app.get<EquipmentController>(EquipmentController);
+    equipmentRepository = app.get<Repository<Equipment>>(
+      EQUIPMENT_REPOSITORY_TOKEN,
+    );
   });
 
   describe('searchEquipments', () => {
-    it('should work', () => {
-      const controller = app.get<EquipmentController>(EquipmentController);
-      expect(controller.searchEquipments(10)).toBe('Equipment search results');
+    it('should work', async () => {
+      jest.spyOn(equipmentRepository, 'find').mockImplementationOnce(() =>
+        Promise.resolve([
+          {
+            equipmentNumber: 1,
+            address: '',
+            contractStartDate: new Date(),
+            contractEndDate: new Date(),
+            status: EquipmentStatus.Running,
+          },
+        ]),
+      );
+      const results = await controller.searchEquipments(10);
+      expect(results).toEqual([
+        {
+          equipmentNumber: 1,
+          address: '',
+          contractStartDate: new Date(),
+          contractEndDate: new Date(),
+          status: EquipmentStatus.Running,
+        },
+      ]);
     });
   });
-
-  describe('getEquipment', () => {
-    it('should work', () => {
-      const controller = app.get<EquipmentController>(EquipmentController);
-      expect(controller.getEquipment(1)).toBe('Equipment Details');
-    });
-  });
-
-  describe('createEquipment', () => {
-    it('should work', () => {
-      const controller = app.get<EquipmentController>(EquipmentController);
-      expect(controller.createEquipment()).toBe('Equipment created');
+  describe('searchEquipments', () => {
+    it('should work', async () => {
+      jest.spyOn(equipmentRepository, 'find').mockImplementationOnce(() =>
+        Promise.resolve([
+          {
+            equipmentNumber: 1,
+            address: '',
+            contractStartDate: new Date(),
+            contractEndDate: new Date(),
+            status: EquipmentStatus.Running,
+          },
+        ]),
+      );
+      const results = await controller.searchEquipments(10);
+      expect(results).toEqual([
+        {
+          equipmentNumber: 1,
+          address: '',
+          contractStartDate: new Date(),
+          contractEndDate: new Date(),
+          status: EquipmentStatus.Running,
+        },
+      ]);
     });
   });
 });
